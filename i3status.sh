2ab6ede8e7807c580a408df6a29dc8e25f2d3003
Paweł Szulczewski <pawel@szulczewski.org>
2015-03-08 13:02:50 +0100

    upgraded mocp things

--- i3status.sh
+++ i3status.sh
@@ -8,10 +8,6 @@ do
     C0="C0: `sensors | grep \"Core 0:\" | perl -ne 'if ($_ =~ m/(\+.*)(.*)/) {print \"$1\n\";}' | cut -d\" \" -f1`"
     C2="C1: `sensors | grep \"Core 2:\" | perl -ne 'if ($_ =~ m/(\+.*)(.*)/) {print \"$1\n\";}' | cut -d\" \" -f1`"
 
-    ARTIST=`mocp -i | grep Artist | cut -d " " -f 2-`
-    SONGTITLE=`mocp -i | grep SongTitle | cut -d " " -f 2-`
-    CURRENTTIME=`mocp -i | grep CurrentTime | cut -d " " -f 2-`
-    TOTALTIME=`mocp -i | grep TotalTime | cut -d " " -f 2-`
-
-    echo "$ARTIST - $SONGTITLE ($CURRENTTIME - $TOTALTIME) | $C0 | $C2 | $line"
+    MOCP=`mocp -Q "%artist - %song (%ct - %tt)"`
+    echo "$MOCP | $C0 | $C2 | $line"
 done
