c2082c2eb922a24b076fe4f6917e3209eab0ee9b
Paweł Szulczewski <pawel@szulczewski.org>
2015-03-08 12:26:18 +0100

    update, added mocp info

--- i3status.sh
+++ i3status.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 IFS=$'\n'
-FILE=http://www.if.pw.edu.pl/~meteo/index.php
 
 i3status -c /home/suzuki/.i3/i3status.conf | while :
 do 
     read line
-     for i in `curl -s $FILE | grep 'ff7777'`
-     do    
-	TEMP=`echo $i | perl -ne 'if ($_ =~ m/\&nbsp;(.*?)&nbsp;/) {print "$1"};'`
-	TEMP_VAL=`echo $TEMP | iconv -f iso-8859-2 -t utf-8`
-	C0="C0: `sensors | grep \"Core 0:\" | perl -ne 'if ($_ =~ m/(\+.*)(.*)/) {print \"$1\n\";}' | cut -d\" \" -f1`"
-	C2="C1: `sensors | grep \"Core 2:\" | perl -ne 'if ($_ =~ m/(\+.*)(.*)/) {print \"$1\n\";}' | cut -d\" \" -f1`"
-	DATA=`date "+%a, %F, %H:%M"`
-	echo "$line | $TEMP_VAL | $C0 | $C2 | $DATA"
-	#echo "$line | $TEMP_VAL | $C0 | $C2 | $DATA"
-    done
-done
+    
+    C0="C0: `sensors | grep \"Core 0:\" | perl -ne 'if ($_ =~ m/(\+.*)(.*)/) {print \"$1\n\";}' | cut -d\" \" -f1`"
+    C2="C1: `sensors | grep \"Core 2:\" | perl -ne 'if ($_ =~ m/(\+.*)(.*)/) {print \"$1\n\";}' | cut -d\" \" -f1`"
 
+    ARTIST=`mocp -i | grep Artist | cut -d " " -f 2-`
+    SONGTITLE=`mocp -i | grep SongTitle | cut -d " " -f 2-`
+    CURRENTTIME=`mocp -i | grep CurrentTime | cut -d " " -f 2-`
+    TOTALTIME=`mocp -i | grep TotalTime | cut -d " " -f 2-`
+
+    echo "$ARTIST - $SONGTITLE ($CURRENTTIME - $TOTALTIME) | $C0 | $C2 | $line"
+done
